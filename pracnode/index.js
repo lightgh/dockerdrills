93c326b23a53b5f09ca4010c358e177916516bd9
Chinaka I Light <chinakalight@googlemail.com>
2023-06-21 13:11:20 -0500

    node docker build run commit

---
+++ pracnode/index.js
@@ -0,0 +1,31 @@
+const  HTTP_PORT = 80;
+
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+
+
+var sourcesDirectory = path.resolve(__dirname, 'www');
+var app = express();
+
+app.use(cors());
+
+app.get('/v1/square/:value', function(req, res){
+    const value = req.params.value;
+    const square = Math.pow(value, 2);
+
+    res.send({
+        value,
+        square
+    });
+});
+
+app.use(express.static(sourcesDirectory, {
+    index: 'index.htm',
+    exrensions: ['htm']
+}));
+
+var server = require('http').createServer(app);
+server.listen(HTTP_PORT);
+
+console.log(`Listening on http://localhost:${HTTP_PORT}`);
