877313e8c8dbc4fd4e95264aa5938b6b31f5fdd4
Chinaka I Light <chinakalight@googlemail.com>
2023-06-23 18:41:09 -0500

    pracnetcore dotnet core

---
+++ pracnetcore/Startup.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjections;
+
+
+namespace aspnet_core
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseDefaultFiles();
+            app.UseMvc();
+        }
+
+    }
+}
